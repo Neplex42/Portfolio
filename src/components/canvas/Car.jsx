f8e22e6916b1e7a42881423688509d053016a7e6
JordanDesktop <jordan.david01234@gmail.com>
2023-10-10 22:39:39 +0200

    chore(package.json): add lamina package as a dependency to enable the use of lamina library for advanced materials and effects feat(Car.jsx): - Add support for depth buffer using the useDepthBuffer hook from drei library to enable depth-based effects - Adjust the position and intensity of the hemisphereLight and spotLight to improve lighting - Increase the intensity of the pointLight to enhance the overall lighting - Adjust the scale, position, and rotation of the car model to improve its appearance - Add MovingSpot component to create moving spotlights with dynamic positions based on mouse movement - Add ambientLight and spotLight components to enhance the lighting in the scene - Add Environment component to provide a preset environment for the scene - Update camera position to improve the view of the scene

--- src/components/canvas/Car.jsx
+++ src/components/canvas/Car.jsx
@@ -1,30 +1,37 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Vector3 } from 'three'
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF, Lightformer, Float, SpotLight, useDepthBuffer, Environment  } from "@react-three/drei";
+import { LayerMaterial, Color, Depth } from 'lamina'
+
 
 import CanvasLoader from "../Loader";
 
+
 const Car = ({ isMobile }) => {
   const car = useGLTF("./bmwCar/scene.gltf");
+  const depthBuffer = useDepthBuffer({ frames: 1 })
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={5} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
+        position={[0, -3.45, -1.5]}
+        angle={.6}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={10} />
       <primitive
         object={car.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 3.5}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.45, -1.5]}
+        rotation={[-0.01, .1, -0.1]}
       />
+      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[3, 3, 2]} />
+      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[1, 3, 0]} />
     </mesh>
   );
 };
@@ -58,7 +65,7 @@ const CarCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 15], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -68,11 +75,30 @@ const CarCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Car isMobile={isMobile} />
+        <ambientLight intensity={0.2} color="#ffcc88" />
+        <spotLight
+          position={[0, -3.45, -1.5]}
+          angle={0.3}
+          penumbra={0.5}
+          intensity={0.5}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <Environment preset="warehouse" />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
 };
+
+function MovingSpot({ vec = new Vector3(), ...props }) {
+  const light = useRef()
+  const viewport = useThree((state) => state.viewport)
+  useFrame((state) => {
+    light.current.target.position.lerp(vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0), 0.1)
+    light.current.target.updateMatrixWorld()
+  })
+  return <SpotLight castShadow ref={light} penumbra={1} distance={6} angle={0.35} attenuation={5} anglePower={4} intensity={2} {...props} />
+}
 
 export default CarCanvas;
