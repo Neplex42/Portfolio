b976e129c1edc0425521416ec4d5b3d0569dc955
JordanDesktop <jordan.david01234@gmail.com>
2023-10-11 01:00:03 +0200

    fix(Car.jsx): change spotLights to be passed as a prop to Car component to allow for dynamic configuration of spot lights feat(Car.jsx): add functionality to randomize spot light intensity every 2 seconds to create a blinking effect

--- src/components/canvas/Car.jsx
+++ src/components/canvas/Car.jsx
@@ -5,42 +5,36 @@ import { OrbitControls, Preload, useGLTF, SpotLight, Environment } from "@react-
 
 import CanvasLoader from "../Loader";
 
-const Car = ({ isMobile }) => {
+const Car = ({ isMobile, spotLights }) => {
   const car = useGLTF("./bmwCar/scene.gltf");
-  
+
   return (
     <mesh>
-      <hemisphereLight intensity={5} groundColor='black' />
-      <SpotLight
-        position={[15, 3, 5]}
-        angle={Math.PI / 2}
-        penumbra={0.1}
-        intensity={300}
-        distance={40}
-        castShadow
-        shadow-mapSize={1024}
-        target={car.scene}
-      />
-      <SpotLight
-        position={[2, 4, 12]}
-        angle={Math.PI / 2}
-        penumbra={0.1}
-        intensity={300}
-        distance={40}
-        castShadow
-        shadow-mapSize={1024}
-        target={car.scene}
-      />
+      <hemisphereLight intensity={5} groundColor="black" />
+      {spotLights.map((light, index) => (
+        <SpotLight
+          key={index}
+          position={light.position}
+          angle={Math.PI / 2}
+          penumbra={0.1}
+          intensity={light.intensity}
+          distance={50}
+          castShadow
+          shadow-mapSize={1024}
+          target={car.scene}
+          color={light.color}
+        />
+      ))}
       <pointLight intensity={30} />
       <primitive
         object={car.scene}
         scale={isMobile ? 0.7 : 3.5}
         position={isMobile ? [0, -3, -2.2] : [0, -3.45, -1.5]}
-        rotation={[-0.01, .1, -0.1]}
+        rotation={[-0.01, 0.1, -0.1]}
       />
     </mesh>
   );
-};
+}
 
 const CarCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -60,27 +54,71 @@ const CarCanvas = () => {
     };
   }, []);
 
+  const [spotLights, setSpotLights] = useState([
+    {
+      position: [15, 3, 5],
+      color: "white",
+      intensity: 300,
+    },
+    {
+      position: [10, 5, -5],
+      color: "yellow",
+      intensity: 300,
+    },
+    {
+      position: [2, 4, 12],
+      color: "#6a5acd",
+      intensity: 300,
+    },
+    {
+      position: [2, 4, 12],
+      color: "#6a5acd",
+      intensity: 300,
+    },
+    {
+      position: [-3, 2, 13],
+      color: "red",
+      intensity: 300,
+    },
+  ]);
+
+  // Fonction pour faire clignoter aléatoirement les lumières
+  const randomizeSpotLightIntensity = () => {
+    const newSpotLights = [...spotLights];
+    newSpotLights.forEach((light) => {
+      light.intensity = Math.random() < 0.5 ? 10 : 300; // Valeurs d'intensité aléatoires
+    });
+    setSpotLights(newSpotLights);
+  };
+
+  useEffect(() => {
+    // Utilisez un intervalle pour changer l'intensité des lumières toutes les 2 secondes
+    const interval = setInterval(randomizeSpotLightIntensity, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 15], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Car isMobile={isMobile} />
-        <ambientLight intensity={0.2} color="#ffcc88" />
-        <Environment preset="night" />
-      </Suspense>
+      <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Car isMobile={isMobile} spotLights={spotLights} />
+      <ambientLight intensity={0.2} color="#ffcc88" />
+      <Environment preset="night" />
       <Preload all />
     </Canvas>
   );
-};
+}
 
 export default CarCanvas;
