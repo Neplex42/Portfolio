5bb6f31f9658a9d865523058e22ad6d23d233765
JordanDesktop <jordan.david01234@gmail.com>
2023-10-14 17:01:20 +0200

    chore(package.json): add react-device-detect package as a dependency fix(Hero.jsx): fix window reference error by using react-device-detect to determine if the device is mobile fix(Car.jsx): fix syntax error by removing unnecessary indentation before OrbitControls component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,11 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { CarCanvas } from "./canvas";
 
+import { DeviceDetect } from "react-device-detect";
+
 const Hero = () => {
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
@@ -24,7 +28,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <CarCanvas />
+      {!isMobile && <CarCanvas />}
 
       <div className="absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center">
         <a href="#about">
