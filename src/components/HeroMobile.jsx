a0cf950e663cc5328af729fabeca0a2d65865c64
JordanDesktop <jordan.david01234@gmail.com>
2023-10-17 19:38:14 +0200

    test pour mobile

--- src/components/HeroMobile.jsx
+++ src/components/HeroMobile.jsx
@@ -7,7 +7,7 @@ import { DeviceDetect } from "react-device-detect";
 
 const HeroMobile = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-[60vh] mx-auto`}>
       <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
 
         <div className='flex flex-col justify-center items-center mt-5'>
@@ -26,9 +26,7 @@ const HeroMobile = () => {
         </div>
       </div>
 
-      <img className="w-full mx-auto pt-[300px]" src="./public/Capture_bmw.png" alt="" />
-
-      <div className="absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-14 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev animate={{y: [0, 24, 0]}} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop'}} className="w-3 h-3 rounded-full bg-secondary mb-1"/>
