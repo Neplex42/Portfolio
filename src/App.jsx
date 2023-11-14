d0944ccbfb20b87e4966d93669c1b8b9afcebafa
JordanEntr <76078411+Neplex42@users.noreply.github.com>
2023-11-14 11:58:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter } from 'react-router-dom'; 
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { isMobile, isBrowser } from 'react-device-detect';
+import { getGPUTier } from 'detect-gpu';
 
 import {
   About,
@@ -10,26 +13,48 @@ import {
   Tech,
   Works,
   StarsCanvas
-} from './components'; // Importe les composants de l'application depuis le dossier 'components'.
+} from './components';
 
-import React, { useEffect } from 'react'; // Importe React et la fonction useEffect pour les effets secondaires.
+const App = () => {
+  const [hasDedicatedGPU, setHasDedicatedGPU] = useState(false);
 
-const App = () => { //App est utiliser dans main.jsx
-  useEffect(() => { // Dans ce cas, useEffect est utilisé pour exécuter du code lorsque le composant App est monté
-    if (performance.navigation.type === 1) { //vérifie si le type de navigation est un rechargement de la page.
-      window.location.href = '/'; // Retour vers la page d'accueil 
+  useEffect(() => {
+    const checkGPU = async () => {
+      try {
+        const gpuTier = await getGPUTier();
+        // Utilisez la valeur de gpuTier pour prendre des décisions, par exemple :
+        if (gpuTier >= 1) {
+          // Le GPU est suffisamment puissant, affichez des éléments 3D
+          setHasDedicatedGPU(true);
+          console.log('Afficher des éléments 3D');
+        } else {
+          // Le GPU n'est pas assez puissant, affichez une alternative
+          setHasDedicatedGPU(false);
+          console.log('Afficher une alternative sans éléments 3D');
+        }
+      } catch (error) {
+        console.error('Erreur lors de la détection du GPU :', error);
+        // En cas d'erreur, gérer la situation en conséquence
+      }
+    };
+
+    checkGPU();
+  }, []);
+
+  useEffect(() => {
+    if (performance.navigation.type === 1) {
+      window.location.href = '/';
     }
-  }, []); // Le fait de passer une liste vide [] comme deuxième argument à useEffect signifie que cette vérification et redirection ne se produit qu'une seule fois lors du montage initial du composant App, que l'effet ne dépend d'aucune variable particulière et doit être exécuté uniquement lors du montage du composant.
+  }, []);
 
-  const isMobile = window.innerWidth <= 450; // Vérifie si la largeur de la fenêtre est inférieure ou égale à 450 pixels, ce qui détermine si l'utilisateur est sur un appareil mobile.
+  const isMobile = window.innerWidth <= 450;
 
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          {!isMobile && <Hero />} {/* Rend le composant 'Hero' s'il ne s'agit pas d'un appareil mobile. */}
-          {isMobile && <HeroMobile />} {/* Rend le composant 'HeroMobile' s'il s'agit d'un appareil mobile. */}
+          {!isMobile && hasDedicatedGPU ? <Hero /> : <HeroMobile />}
         </div>
         <About />
         <Experience />
@@ -44,4 +69,4 @@ const App = () => { //App est utiliser dans main.jsx
   );
 }
 
-export default App; // Exporte le composant App en tant que composant racine de l'application.
+export default App;
