d51804d9a410bfcefcc2023a7b9eec57e6e34377
JordanEntr <76078411+Neplex42@users.noreply.github.com>
2023-11-14 13:25:57 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
@@ -6,10 +6,37 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import { isMobile, isBrowser } from 'react-device-detect';
+import { getGPUTier } from 'detect-gpu';
 
-const isMobile = window.innerWidth <= 450;
+// const isMobile = window.innerWidth <= 450;
 
 const Contact = () => {
+  const [hasDedicatedGPU, setHasDedicatedGPU] = useState(false);
+
+  useEffect(() => {
+    const checkGPU = async () => {
+      try {
+        const gpuTier = await getGPUTier();
+        // Utilisez la valeur de gpuTier pour prendre des décisions, par exemple :
+        if (gpuTier >= 1) {
+          // Le GPU est suffisamment puissant, affichez des éléments 3D
+          setHasDedicatedGPU(true);
+          console.log('Afficher des éléments 3D');
+        } else {
+          // Le GPU n'est pas assez puissant, affichez une alternative
+          setHasDedicatedGPU(false);
+          console.log('Afficher une alternative sans éléments 3D');
+        }
+      } catch (error) {
+        console.error('Erreur lors de la détection du GPU :', error);
+        // En cas d'erreur, gérer la situation en conséquence
+      }
+    };
+
+    checkGPU();
+  }, []);
+
   const formRef = useRef();
   const [form, setForm] = useState({
     name: "",
@@ -68,11 +95,11 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`xl:mt-12 flex-auto xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='flex-[2.75] bg-black-100 mx-20 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Prendre contact</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -129,7 +156,8 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
-        {!isMobile && <EarthCanvas />}
+        {/* {!isMobile && <EarthCanvas />} */}
+        {!isMobile && hasDedicatedGPU ? <EarthCanvas /> : null}
         {isMobile && <></>}
       </motion.div>
     </div>
